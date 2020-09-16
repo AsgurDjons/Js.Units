0b629771b6a0d18b8a13798a2343eaf06556b2e5
AsgurDjons <61666677+AsgurDjons@users.noreply.github.com>
2020-09-16 19:02:23 +0300

    21unit

--- dz_21_blank/dz_21_blank/unit_21.js
+++ dz_21_blank/dz_21_blank/unit_21.js
@@ -12,10 +12,10 @@ document.querySelector('.div-1').addEventListener('touchstart', t1);
 // Task 2 ============================================
 /* Создайте блок div-2. Добавьте на него событие touchstart. 
 Выведите в out-2 число срабатываний события. */
-
-function t2(e) {
-console.log(e);
-    document.querySelector('.out-2').innerHTML = e.touches.length;
+let num = 1;
+function t2() {
+    document.querySelector('.out-2').innerHTML = num;
+    num++;
 }
 
 document.querySelector('.div-2').addEventListener('touchstart', t2);
@@ -25,11 +25,16 @@ document.querySelector('.div-2').addEventListener('touchstart', t2);
 /*  Создайте блок div-3_1 и div-3_2. Добавьте на них событие touchstart. 
 Выведите в out-3 номер блока 1 или 2 на котором сработало событие. */
 
-function t3() {
-
+function t3(e) {
+    if (e.target == document.querySelector('.div-3_1')) {
+        document.querySelector('.out-3').textContent = '1';
+    } else if(e.target == document.querySelector('.div-3_2')) {
+        document.querySelector('.out-3').textContent = '2';
+    }
 }
 
-// ваше событие здесь!!!
+document.querySelector('.div-3_1').addEventListener('touchstart', t3);
+document.querySelector('.div-3_2').addEventListener('touchstart', t3);
 
 
 // Task 4 ============================================
@@ -37,85 +42,99 @@ function t3() {
 При событии происходит вывод текста touch в out-4.  */
 
 function t4() {
-
+    document.querySelector('.div-4').textContent += 'touch';
 }
 
-// ваше событие здесь!!!
+document.querySelector('.b-4').addEventListener('click', () => {
+    document.querySelector('.div-4').addEventListener('touchstart', t4);
+});
 
 // Task 5 ============================================
 /*  Дана кнопка b-5. При ее нажатии очищайте событие ontouchstart на блоке div-4. */
 
 function t5() {
-
+    document.querySelector('.div-4').removeEventListener('touchstart', t4);
 }
 
-// ваше событие здесь!!!
+document.querySelector('.b-5').addEventListener('click', t5);
 
 // Task 6 ============================================
 /*  Добавьте событие ontouchend на div-4. При его срабатывании выведите в out-6 слово touchend. */
 
 function t6() {
-
+    document.querySelector('.out-6').textContent = 'touchend';
 }
 
-// ваше событие здесь!!!
+document.querySelector('.div-4').addEventListener('touchend', t6);
 
 
 // Task 7 ============================================
 /*  Дан блок div-7. Добавьте событие touch, при срабатывании которого окрашивайте блок в красный цвет. */
 
 function t7() {
-
+    document.querySelector('.div-7').style.cssText = 'background:red;';
 }
 
-// ваше событие здесь!!!
+document.querySelector('.div-7').addEventListener('touchstart', t7);
 
 // Task 8 ============================================
-/*  Дан блок div-8. Добавьте на него событие touch, которое при срабатывании окрашивает блок случаным цветом из массива a8=[red, green, blue, orange, pink, yellow] */
+/*  Дан блок div-8. Добавьте на него событие touch, которое при срабатывании окрашивает блок 
+случаным цветом из массива a8=[red, green, blue, orange, pink, yellow] */
 
 function t8() {
-
+    let a8 = ['red', 'green', 'blue', 'orange', 'pink', 'yellow'],
+        randomColor = a8[Math.floor(Math.random() * a8.length)];
+    document.querySelector('.div-8').style.cssText = `background:${randomColor};`;
 }
 
-// ваше событие здесь!!!
+document.querySelector('.div-8').addEventListener('touchstart', t8);
 
 
 // Task 9 ============================================
 /* Дан блок div-9. Добавьте событие ontouch. Выводите количество одновременных касаний в out-9. */
 
-function t9() {
-
+function t9(e) {
+    document.querySelector('.out-9').innerHTML = e.touches.length;
 }
 
-// ваше событие здесь!!!
+document.querySelector('.div-9').addEventListener('touchstart', t9);
 
 
 // Task 10 ============================================
 /*  Дан блок div-10. Добавьте на него событие touchmove. При срабатывании события - увеличивайте его ширину на 1. */
-
+ let width = 75;
 function t10() {
-
+    document.querySelector('.div-10').style.cssText = `width: ${width}px;`;
+    width+=1;
 }
 
-// ваше событие здесь!!!
+document.querySelector('.div-10').addEventListener('touchmove', t10);
 
 // Task 11 ============================================
 /*  Дан блок div-11. Добавьте на него событие touch. При срабатывании выводите радиус события radiusX, radiusY. */
 
-function t11() {
-
+function t11(e) {
+    console.log(e);
+    var touch = e.changedTouches.item(0);
+    document.querySelector('.out-11').innerHTML = `${touch.radiusX}, ${touch.radiusY}`;
 }
 
-// ваше событие здесь!!!
+document.querySelector('.div-11').addEventListener('touchstart', t11);
 
 // Task 12 ============================================
 /*  Мини проект. Ознакомьтесь с версткой в задании 12.
 <p>Изучите html код внутри div-12-wrapper.</p>
-<p>1. Добавьте на кнопку nex событие click, touch так, чтобы при событии запускалась функция nextFunction и активным становилось изображение следующее за выделенным классом active-img (рамкой). Соответственно, на активном изображении появляется рамка, а остальные - лишаются рамки.</p>
-<p>2. Добавьте на кнопку prev событие click, touch так, чтобы при событии запускалась функция prevFunction и активным становилось изображение до выделенного классом active-img (рамкой). Соответственно, на активном изображении появляется рамка, а остальные - лишаются рамки.</p>
-<p>3. Учтите краевые эффекты - когда мы доходим до конца или начала, то нажатие кнопки должно приводить к перемещению рамки в начало или конец изображений.</p>
+<p>1. Добавьте на кнопку nex событие click, touch так, чтобы при событии запускалась функция nextFunction и 
+активным становилось изображение следующее за выделенным классом active-img (рамкой). Соответственно, на 
+активном изображении появляется рамка, а остальные - лишаются рамки.</p>
+<p>2. Добавьте на кнопку prev событие click, touch так, чтобы при событии запускалась функция prevFunction и 
+активным становилось изображение до выделенного классом active-img (рамкой). Соответственно, на активном 
+изображении появляется рамка, а остальные - лишаются рамки.</p>
+<p>3. Учтите краевые эффекты - когда мы доходим до конца или начала, то нажатие кнопки должно приводить к 
+перемещению рамки в начало или конец изображений.</p>
 <p>4. Добавьте кнопку reset (функция resetFunction), нажатие которой сбрасывает активное изображение на нулевое. </p>
-<p>5. Добавьте во все действия следующее - в изображении img-12-max заменяется src на активную. Т.е. произошло событие - заменилась главная картинка.</p>
+<p>5. Добавьте во все действия следующее - в изображении img-12-max заменяется src на активную. Т.е. 
+произошло событие - заменилась главная картинка.</p>
 */
 
 const images = document.querySelectorAll('.img-12-min');
