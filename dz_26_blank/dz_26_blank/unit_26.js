7c817f03abdfe2475690547fdfaae4637b64b348
AsgurDjons <61666677+AsgurDjons@users.noreply.github.com>
2020-09-25 22:50:15 +0300

    27 unit

--- dz_26_blank/dz_26_blank/unit_26.js
+++ dz_26_blank/dz_26_blank/unit_26.js
@@ -23,10 +23,16 @@ document.querySelector('.b-1').addEventListener('click', t1);
 Запускаться функция должна по нажатию b-2. */
 
 function t2() {
-
+    fetch('http://getpost.itgid.info/index2.php?auth=5ADcB96BA48d3f80&action=2&name=Zhenya', {
+        method: 'GET',
+    })
+    .then(response => response.text())
+    .then(response => {
+        document.querySelector('.out-2').textContent = response;
+    });
 }
 
-// ваше событие здесь!!!
+document.querySelector('.b-2').addEventListener('click', t2);
 
 
 // Task 3 ============================================
@@ -36,10 +42,14 @@ function t2() {
 Запускаться функция должна по нажатию b-3. */
 
 function t3() {
-
+    fetch(`http://getpost.itgid.info/index2.php?auth=5ADcB96BA48d3f80&action=3&num1=10&num2=3`, {
+        method: 'GET',  
+    }).then(response => response.text()).then(response => {
+        document.querySelector('.out-3').textContent = response;
+    });
 }
 
-// ваше событие здесь!!!
+document.querySelector('.b-3').addEventListener('click', t3);
 
 
 // Task 4 ============================================
@@ -50,10 +60,14 @@ function t3() {
 Запускаться функция должна по нажатию b-4. */
 
 function t4() {
-
+    fetch(`http://getpost.itgid.info/index2.php?auth=5ADcB96BA48d3f80&action=4&num1=1&num2=10`, {
+        method: 'GET',  
+    }).then(response => response.text()).then(response => {
+        document.querySelector('.out-4').textContent = response;
+    });
 }
 
-// ваше событие здесь!!!
+document.querySelector('.b-4').addEventListener('click', t4);
 
 // Task 5 ============================================
 /*  Отправьте GET запрос (fetch) на сайт http://getpost.itgid.info/index2.php. 
@@ -62,10 +76,15 @@ function t4() {
 Запускаться функция должна по нажатию b-5. */
 
 function t5() {
-
+    const num = 5;
+    fetch(`http://getpost.itgid.info/index2.php?auth=5ADcB96BA48d3f80&action=${num}`, {
+        method: 'GET',  
+    }).then(response => response.text()).then(response => {
+        document.querySelector('.out-5').textContent = response;
+    });
 }
 
-// ваше событие здесь!!!
+document.querySelector('.b-5').addEventListener('click', t5);
 
 // Task 6 ============================================
 /*  Отправьте GET запрос (fetch) на сайт http://getpost.itgid.info/index2.php. 
@@ -74,10 +93,14 @@ function t5() {
 Выведите в out-6 результат. Запускаться функция должна по нажатию b-6. */
 
 function t6() {
-
+    fetch('http://getpost.itgid.info/index2.php?auth=5ADcB96BA48d3f80&action=6&num1=5&num2=14', {
+        method: 'GET',  
+    }).then(response => response.text()).then(response => {
+        document.querySelector('.out-6').textContent = response;
+    });
 }
 
-// ваше событие здесь!!!
+document.querySelector('.b-6').addEventListener('click', t6);
 
 
 // Task 7 ============================================
@@ -87,10 +110,14 @@ function t6() {
 Запускаться функция должна по нажатию b-7. */
 
 function t7() {
-
+    fetch('http://getpost.itgid.info/index2.php?auth=5ADcB96BA48d3f80&action=7', {
+        method: 'GET',  
+    }).then(response => response.text()).then(response => {
+        document.querySelector('.out-7').textContent = response;
+    });
 }
 
-// ваше событие здесь!!!
+document.querySelector('.b-7').addEventListener('click', t7);
 
 // Task 8 ============================================
 /* Отправьте GET запрос (fetch) на сайт http://getpost.itgid.info/index2.php. 
@@ -99,10 +126,14 @@ function t7() {
 Выведите в out-8 результат. Запускаться функция должна по нажатию b-8.*/
 
 function t8() {
-
+    fetch('http://getpost.itgid.info/index2.php?auth=5ADcB96BA48d3f80&action=8&year=1992', {
+        method: 'GET',  
+    }).then(response => response.text()).then(response => {
+        document.querySelector('.out-8').textContent = response;
+    });
 }
 
-// ваше событие здесь!!!
+document.querySelector('.b-8').addEventListener('click', t8);
 
 
 // Task 9 ============================================
@@ -113,10 +144,14 @@ function t8() {
 Запускаться функция должна по нажатию b-9. */
 
 function t9() {
-
+    fetch('http://getpost.itgid.info/index2.php?auth=5ADcB96BA48d3f80&action=9&m=1&d=1&y=1', {
+        method: 'GET',  
+    }).then(response => response.text()).then(response => {
+        document.querySelector('.out-9').textContent = response;
+    });
 }
 
-// ваше событие здесь!!!
+document.querySelector('.b-9').addEventListener('click', t9);
 
 
 // Task 10 ============================================
@@ -128,8 +163,17 @@ function t9() {
 */
 
 function t10() {
-
+    fetch('http://getpost.itgid.info/index2.php?auth=5ADcB96BA48d3f80&action=1', {
+        method: 'POST',
+    })
+    .then(response => response.text())
+    .then(response => {
+        document.querySelector(".out-10").textContent = response;
+    });
 }
+
+document.querySelector('.b-10').addEventListener('click', t10);
+
 // Task 11 ============================================
 /*  Отправьте POST запрос (fetch) на сайт http://getpost.itgid.info/index2.php. 
 В качестве action укажите 2. Добавьте параметр name с вашим именем на латинице. 
@@ -137,11 +181,16 @@ function t10() {
 Выведите в out-11 результат. Запускаться функция должна по нажатию b-11. */
 
 function t11() {
-
+    fetch('http://getpost.itgid.info/index2.php?auth=5ADcB96BA48d3f80&action=2&name=Zhenya', {
+        method: 'POST',
+    })
+    .then(response => response.text())
+    .then(response => {
+        document.querySelector('.out-11').textContent = response;
+    });
 }
 
-// ваше событие здесь!!!
-
+document.querySelector('.b-11').addEventListener('click', t11);
 // Task 12 ============================================
 /*  Отправьте POST запрос (fetch) на сайт http://getpost.itgid.info/index2.php. 
 В качестве action укажите 3. Добавьте параметр num1 и num2 содержащие числа. 
@@ -149,10 +198,14 @@ function t11() {
 Выведите в out-12 результат. Запускаться функция должна по нажатию b-12.*/
 
 function t12() {
-
+    fetch(`http://getpost.itgid.info/index2.php?auth=5ADcB96BA48d3f80&action=3&num1=10&num2=3`, {
+        method: 'POST',  
+    }).then(response => response.text()).then(response => {
+        document.querySelector('.out-12').textContent = response;
+    });
 }
 
-// ваше событие здесь!!!
+document.querySelector('.b-12').addEventListener('click', t12);
 
 // Task 13 ============================================
 /*  Отправьте POST запрос (fetch) на сайт http://getpost.itgid.info/index2.php. 
@@ -162,10 +215,14 @@ function t12() {
 Запускаться функция должна по нажатию b-13.*/
 
 function t13() {
-
+    fetch('http://getpost.itgid.info/index2.php?auth=5ADcB96BA48d3f80&action=4&num1=1&num2=10', {
+        method: 'POST',  
+    }).then(response => response.text()).then(response => {
+        document.querySelector('.out-13').textContent = response;
+    });
 }
 
-// ваше событие здесь!!!
+document.querySelector('.b-13').addEventListener('click', t13);
 
 // Task 14 ============================================
 /*  Отправьте POST запрос (fetch) на сайт http://getpost.itgid.info/index2.php. 
@@ -174,10 +231,15 @@ function t13() {
 Запускаться функция должна по нажатию b-14.*/
 
 function t14() {
-
+    const num = 5;
+    fetch(`http://getpost.itgid.info/index2.php?auth=5ADcB96BA48d3f80&action=${num}`, {
+        method: 'POST',  
+    }).then(response => response.text()).then(response => {
+        document.querySelector('.out-14').textContent = response;
+    });
 }
 
-// ваше событие здесь!!!
+document.querySelector('.b-14').addEventListener('click', t14);
 
 // Task 15============================================
 /*  Отправьте POST запрос (fetch) на сайт http://getpost.itgid.info/index2.php. 
@@ -186,10 +248,14 @@ function t14() {
 Выведите в out-15 результат. Запускаться функция должна по нажатию b-15. */
 
 function t15() {
-
+    fetch('http://getpost.itgid.info/index2.php?auth=5ADcB96BA48d3f80&action=6&num1=5&num2=14', {
+        method: 'POST',  
+    }).then(response => response.text()).then(response => {
+        document.querySelector('.out-15').textContent = response;
+    });
 }
 
-// ваше событие здесь!!!
+document.querySelector('.b-15').addEventListener('click', t15);
 
 // Task 16 ============================================
 /*  Отправьте POST запрос (fetch) на сайт http://getpost.itgid.info/index2.php. 
@@ -198,10 +264,14 @@ function t15() {
 Запускаться функция должна по нажатию b-16. */
 
 function t16() {
-
+    fetch('http://getpost.itgid.info/index2.php?auth=5ADcB96BA48d3f80&action=7', {
+        method: 'POST',  
+    }).then(response => response.text()).then(response => {
+        document.querySelector('.out-16').textContent = response;
+    });
 }
 
-// ваше событие здесь!!!
+document.querySelector('.b-16').addEventListener('click', t16);
 
 // Task 17 ============================================
 /*  Отправьте POST запрос (fetch) на сайт http://getpost.itgid.info/index2.php. 
@@ -210,10 +280,14 @@ function t16() {
 Выведите в out-17 результат. Запускаться функция должна по нажатию b-17. */
 
 function t17() {
-
+    fetch('http://getpost.itgid.info/index2.php?auth=5ADcB96BA48d3f80&action=8&year=1992', {
+        method: 'POST',  
+    }).then(response => response.text()).then(response => {
+        document.querySelector('.out-17').textContent = response;
+    });
 }
 
-// ваше событие здесь!!!
+document.querySelector('.b-17').addEventListener('click', t17);
 
 // Task 18 ============================================
 /*  Отправьте POST запрос (fetch) на сайт http://getpost.itgid.info/index2.php. 
@@ -222,8 +296,12 @@ function t17() {
 Выведите в out-18 результат. Запускаться функция должна по нажатию b-18. */
 
 function t18() {
-
+    fetch('http://getpost.itgid.info/index2.php?auth=5ADcB96BA48d3f80&action=9&m=1&d=1&y=1', {
+        method: 'POST',  
+    }).then(response => response.text()).then(response => {
+        document.querySelector('.out-18').textContent = response;
+    });
 }
 
-// ваше событие здесь!!!
+document.querySelector('.b-18').addEventListener('click', t18);
 
